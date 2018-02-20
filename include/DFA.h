ec1f80a66cf3c4ab4a459338313fe0585d47bee4
Felix Klauke <info@felix-klauke.de>
2018-02-20 12:31:51 +0100

    Feature/state (#3)
    * Add dfa preparation and add exectuable.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>
    
    * Add state and alphabet.
    
    Signed-off-by: Felix Klauke <info@felix-klauke.de>

--- include/DFA.h
+++ include/DFA.h
@@ -5,4 +5,16 @@
 #ifndef WINCHESTER_DFA_H
 #define WINCHESTER_DFA_H
 
+#include <set>
+#include <State.h>
+
+class DFA {
+public:
+    explicit DFA(const std::set<char> &alphabet, std::set<State> states);
+
+private:
+    std::set<State> states;
+    std::set<char> alphabet;
+};
+
 #endif //WINCHESTER_DFA_H
